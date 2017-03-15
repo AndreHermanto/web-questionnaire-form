eefc8c5fe83ee4adf3042482addc3de2e7134440
Craig McNamara <c.mcnamara@garvan.org.au>
2017-03-15 14:56:21 +1000

    feat(form): add saving

--- src/questionnaires/QuestionnaireFormContainer/QuestionPreview.js
+++ src/questionnaires/QuestionnaireFormContainer/QuestionPreview.js
@@ -1,23 +1,59 @@
 import React from 'react';
+import _ from 'lodash';
 
-export default function QuestionPreview({ question, number, onAnswer }) {
+export default function QuestionPreview({
+  question,
+  number,
+  questionResponse,
+  onAnswer }) {
+  const handleAnswer = (e, answer) => {
+    const target = e.target;
+    const newQuestionResponse = Object.assign({}, questionResponse);
+    if (question.get('type') === 'checkbox') {
+      if (target.checked) {
+        newQuestionResponse.answers.push({
+          id: answer.get('id')
+        });
+      } else {
+        newQuestionResponse.answers = _.reject(newQuestionResponse.answers, { id: answer.get('id') });
+      }
+    }
+    if (question.get('type') === 'radio') {
+      newQuestionResponse.answers = [{
+        id: answer.get('id')
+      }];
+    }
+    if (question.get('type') === 'text') {
+      newQuestionResponse.answers = [{
+        id: answer.get('id'),
+        text: target.value
+      }];
+    }
+    onAnswer(newQuestionResponse);
+  };
+
   let answers = '';
   if (question.get('type') === 'section') {
-    return <h3 style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 16 }}>{question.get('title')}</h3>
+    return <h3 style={{ fontSize: 16, fontWeight: 'bold', marginBottom: 16 }}>{question.get('title')}</h3>;
   }
 
   if (question.get('type') === 'checkbox' || question.get('type') === 'radio' || question.get('type') === 'text') {
-    answers = question.get('answers').map((answer, index) => {
-      return <div className={question.get('type')} key={index}>
+    answers = question.get('answers').map(answer =>
+      <div className={question.get('type')} key={answer.get('id')}>
         <label>
           {question.get('type') !== 'text' &&
           <input
             name={question.get('id')}
             type={question.get('type')}
-            onChange={onAnswer} />
+            onChange={e => handleAnswer(e, answer)}
+          />
           }
           {question.get('type') === 'text' &&
-          <textarea className="form-control" rows="3"></textarea>
+          <textarea
+            className="form-control"
+            rows="3"
+            onChange={e => handleAnswer(e, answer)}
+          />
           }
           {' '}
           {answer.get('text')} {answer.get('goTo') && <small className="text-muted">Go to: {answer.get('goTo')}</small>}
@@ -26,14 +62,14 @@ export default function QuestionPreview({ question, number, onAnswer }) {
           <span className="text-muted">({answer.get('concepts').map(concept => <small key={concept.get('id')} className="text-success">{concept.get('label')}</small>)})</span>
           }
         </label>
-      </div>;
-    });
+      </div>
+    );
   } else {
     answers = 'Unknown type';
   }
 
-  return <div>
+  return (<div>
     <p><strong>{number}. {question.get('question')}</strong></p>
     {answers}
-  </div>
+  </div>);
 }
--- src/questionnaires/QuestionnaireFormContainer/index.js
+++ src/questionnaires/QuestionnaireFormContainer/index.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react';
-import {fromJS} from 'immutable';
+import React, { Component } from 'react';
+import { fromJS } from 'immutable';
+import cuid from 'cuid';
+import _ from 'lodash';
 import genomeConnect from './version';
 import QuestionPreview from './QuestionPreview';
 
@@ -11,17 +13,30 @@ const completed = false;
 class QuestionnaireForm extends Component {
   constructor(props) {
     super(props);
+    const version = fromJS(genomeConnect).update('body', (body) => {
+      return body.map((item) => {
+        if (!item.has('answers')) {
+          return item;
+        }
+        return item.update('answers', answers =>
+          answers.map(answer =>
+            answer.set('id', cuid())
+          )
+        );
+      });
+    });
+
     this.state = {
       questionnaire: fromJS({
-        "id": "eda1fd27-0303-4083-93aa-a01fe8536607",
-        "dateCreated": "Thu Mar 09 2017 02:36:21 GMT+1000 (AEST)",
-        "lastUpdated": "Thu Mar 09 2017 12:42:54 GMT+1000 (AEST)",
-        "status": "molestias",
-        "currentTitle": "Id voluptas cupiditate.",
-        "currentVersionId": "1615a7ad-6f52-4377-aac1-8129c20c9341",
-        "creator": "delectus"
+        id: 'eda1fd27-0303-4083-93aa-a01fe8536607',
+        dateCreated: 'Thu Mar 09 2017 02:36:21 GMT+1000 (AEST)',
+        lastUpdated: 'Thu Mar 09 2017 12:42:54 GMT+1000 (AEST)',
+        status: 'molestias',
+        currentTitle: 'Id voluptas cupiditate.',
+        currentVersionId: '1615a7ad-6f52-4377-aac1-8129c20c9341',
+        creator: 'delectus'
       }),
-      version: fromJS(genomeConnect)
+      version
     };
     this.createResponse = this.createResponse.bind(this);
     this.updateResponse = this.updateResponse.bind(this);
@@ -31,35 +46,41 @@ class QuestionnaireForm extends Component {
   componentDidMount() {
     this.createResponse();
 
-    return fetch(`${process.env.REACT_APP_BASE_URL}/questionnaires/${this.props.params.questionnaireId}`).then(response => response.json()).then(json => json.data).then(questionnaire => {
-      this.setState({questionnaire});
-    }).catch(console.error);
+    return fetch(`${process.env.REACT_APP_BASE_URL}/questionnaires/${this.props.params.questionnaireId}`)
+      .then(response => response.json())
+      .then(json => json.data)
+      .then((questionnaire) => {
+        this.setState({
+          questionnaire
+        });
+      })
+      .catch(console.error);
   }
 
-  updateResponse() {
-    return fetch(`${process.env.REACT_APP_BASE_URL}/responses/${this.state.response.id}`, {
+  updateResponse(questionnaireResponse) {
+    return fetch(`${process.env.REACT_APP_BASE_URL}/responses/${questionnaireResponse.id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(this.state.response)
+      body: JSON.stringify(questionnaireResponse)
     })
     .then(response => response.json())
     .then(json => json.data)
-    .then(response => {
+    .then((response) => {
       this.setState({
-        response: response
+        response
       });
     })
     .catch(console.error);
   }
 
   createResponse() {
-    const response = {
-      userId: userId,
-      questionnaireId: questionnaireId,
-      questionnaireVersionId: versionId,
-      completed: completed,
+    const questionnaireResponse = {
+      userId,
+      questionnaireId,
+      versionId,
+      completed,
       answeredQuestions: []
     };
     return fetch(`${process.env.REACT_APP_BASE_URL}/responses`, {
@@ -67,20 +88,35 @@ class QuestionnaireForm extends Component {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(response)
+      body: JSON.stringify(questionnaireResponse)
     })
     .then(response => response.json())
     .then(json => json.data)
-    .then(response => {
+    .then((response) => {
       this.setState({
-        response: response
+        response
       });
     })
     .catch(console.error);
   }
 
-  handleQuestionAnswered() {
-    this.updateResponse();
+  handleQuestionAnswered(question) {
+    const answeredQuestions = this.state.response.answeredQuestions;
+    const response = this.state.response;
+    const index = _.findIndex(answeredQuestions, { id: question.id });
+    if (index >= 0) {
+      answeredQuestions.splice(index, 1, question);
+      response.answeredQuestions = answeredQuestions;
+    } else {
+      response.answeredQuestions.push(question);
+    }
+
+    // store the change
+    this.setState({
+      response
+    });
+    // send to server
+    this.updateResponse(response);
   }
 
   render() {
@@ -88,13 +124,25 @@ class QuestionnaireForm extends Component {
       <div className="container">
         <h4>Questionnaire Form</h4>
         {this.state.version.get('body').count()}
-        {this.state.version.get('body').map((question, index) =>
-          <QuestionPreview
+        {this.state.version.get('body').map((question, index) => {
+          let questionResponse;
+          if (this.state.response) {
+            questionResponse = _.find(this.state.response.answeredQuestions, { id: question.get('id') }) || {
+              id: question.get('id'),
+              answers: []
+            };
+          } else {
+            questionResponse = { id: question.get('id'), answers: [] };
+          }
+
+          return (<QuestionPreview
             key={question.get('id')}
             number={index + 1}
             question={question}
-            onAnswer={this.handleQuestionAnswered} />
-        )}
+            questionResponse={questionResponse}
+            onAnswer={this.handleQuestionAnswered}
+          />);
+        })}
       </div>
     );
   }
