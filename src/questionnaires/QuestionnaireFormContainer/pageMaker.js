d87f816e6fd65e3aecbfc812795f107ac2d39e94
Craig McNamara <c.mcnamara@garvan.org.au>
2017-04-27 11:29:51 +1000

    replace "questions" with "elements"
    The change makes it easier to read, as "elements" can refer to questions or sections. Previously, everything was a question, even the sections, which was very confusing.

--- src/questionnaires/QuestionnaireFormContainer/pageMaker.js
+++ src/questionnaires/QuestionnaireFormContainer/pageMaker.js
@@ -1,37 +1,37 @@
 import cuid from 'cuid';
 import _ from 'lodash';
 
-function pageMaker(questions) {
-  return questions.reduce((sum, question, index) => {
+function pageMaker(elements) {
+  return elements.reduce((sum, element, index) => {
     let page;
     // new section
-    if (question.type === 'section') {
-      // if its a heading, or the first question
-      if (question.size === 1 || index === 0) {
+    if (element.type === 'section') {
+      // if its a heading, or the first element
+      if (element.size === 1 || index === 0) {
         page = {
           id: cuid(),
-          heading: question.title,
-          sectionId: question.id,
+          heading: element.title,
+          sectionId: element.id,
           questions: []
         };
         sum.push(page);
         return sum;
       }
 
-      if (question.size === 2) {
+      if (element.size === 2) {
         // look at the previous page
         const previousPage = sum[sum.length - 1];
         if (!previousPage.questions.length) {
           // previous page was just a heading, no questions
           // so add to that
-          previousPage.heading2 = question.title;
+          previousPage.heading2 = element.title;
         } else {
           page = {
             id: cuid(),
             heading: previousPage.heading,
-            heading2: question.title,
+            heading2: element.title,
             heading3: undefined,
-            sectionId: question.id,
+            sectionId: element.id,
             questions: []
           };
           sum.push(page);
@@ -39,20 +39,20 @@ function pageMaker(questions) {
         return sum;
       }
 
-      if (question.size === 3) {
+      if (element.size === 3) {
         // look at the previous page
         const previousPage = sum[sum.length - 1];
         if (!previousPage.questions.length) {
           // previous page was just a heading, no questions
           // so add to that
-          previousPage.heading3 = question.title;
+          previousPage.heading3 = element.title;
         } else {
           page = {
             id: cuid(),
             heading: previousPage.heading,
             heading2: previousPage.heading2,
-            heading3: question.title,
-            sectionId: question.id,
+            heading3: element.title,
+            sectionId: element.id,
             questions: []
           };
           sum.push(page);
@@ -62,7 +62,7 @@ function pageMaker(questions) {
     }
 
     // its a question
-    // if (question.type !== 'section') {
+    // if (element.type !== 'section') {
       // first question, so add a page
     if (sum.length === 0) {
       sum.push({
@@ -72,11 +72,11 @@ function pageMaker(questions) {
       });
     }
     page = sum[sum.length - 1];
-    page.questions.push(question);
+    page.questions.push(element);
 
     // has skip logic, so add a new page after this question
-    const hasSkipLogic = _.find(question.answers, answer => answer.goTo);
-    if (hasSkipLogic && index !== questions.length - 1) {
+    const hasSkipLogic = _.find(element.answers, answer => answer.goTo);
+    if (hasSkipLogic && index !== elements.length - 1) {
       sum.push({
         id: cuid(),
         heading: page.heading,
