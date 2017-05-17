18bceb2a2a0410b8fa7cfc3df87a01eef11a9509
Craig McNamara <c.mcnamara@garvan.org.au>
2017-05-18 09:46:13 +1000

    add redux-logger for logging store changes
    See https://github.com/evgenyrodionov/redux-logger

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import reducer from './reducers';
 import './index.css';
 
-//redux
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import PatientPortalApp from './reducers/index.js';
+// redux
+const middleware = [];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
 
-//create patient portal store
-let store = createStore(PatientPortalApp);
-
-/*
-* the code below would be useful for tracking our store state and action 
-*/
-
-store.subscribe((render)=>
-{
-  console.log("store changed", store.getState());
-})
+// create patient portal store
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware) // add logging in as middleware
+);
 
 ReactDOM.render(
   <Provider store={store}>
