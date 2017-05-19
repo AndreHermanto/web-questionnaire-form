98a6d6294e7b6b6ae5be7c26c660f18580905522
Craig McNamara <c.mcnamara@garvan.org.au>
2017-05-19 12:01:33 +1000

    change action name

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 /*
 *action: addQuestionnaires to the form builder
 */
-export function addQuestionnaires(questionnaires) {
+export function fetchQuestionnairesSuccess(questionnaires) {
   return {
-    type: 'ADD_QUESTIONNAIRES',
+    type: 'FETCH_QUESTIONNAIRES_SUCCESS',
     payload: questionnaires
   };
 }
@@ -14,23 +14,23 @@ export function addQuestionnaires(questionnaires) {
 function fetchQuestionnairesRequest() {
   return {
     type: 'FETCH_QUESTIONNAIRES_REQUEST'
-  }
+  };
 }
 
 function fetchQuestionnairesFailure(ex) {
   return {
     type: 'FETCH_QUESTIONNAIRES_FAILURE',
     ex
-  }
+  };
 }
 
 export function fetchQuestionnaires() {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(fetchQuestionnairesRequest())
     return fetch(`${process.env.REACT_APP_BASE_URL}/questionnaires`)
       .then(res => res.json())
       .then(json => json.data)
-      .then(questionnaires => dispatch(addQuestionnaires(questionnaires)))
-      .catch(ex => dispatch(fetchQuestionnairesFailure(ex)))
+      .then(questionnaires => dispatch(fetchQuestionnairesSuccess(questionnaires)))
+      .catch(ex => dispatch(fetchQuestionnairesFailure(ex)));
   }
 }
