a6914bc8ec9e8809229fe62ea0a914e4599d8e11
Luong Khoi Nguyen Nguyen <khoi.nguyen@genome.one>
2017-06-20 14:37:36 +1000

    Add end page for questionnaire after submitting!

--- src/components/QuestionnaireFormSubmitted/index.js
+++ src/components/QuestionnaireFormSubmitted/index.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Grid, Col } from 'react-bootstrap';
+import {
+  getCurrentVersion
+} from '../../reducers';
+import styled from 'styled-components';
+const DisplayText = styled.div`
+  fontSize: 20px; 
+  textAlign: center;
+  fontStyle: ${props => props.isItalic ? 'italic' : 'normal' };
+  fontWeight: ${props => props.isBold ? 'bold' : 'normal'};
+  padding-top: 20px;
+`;
+
+const DisplayButton = styled.div`
+  margin: auto;
+  textAlign: center;
+  marginBottom: 10px;
+  padding-top: 20px;
+`;
 
 class QuestionnaireFormSubmitted extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Thanks!</h1>
-        <p>Your response has been submitted.</p>
-      </div>
+      <Grid>
+      {
+        this.props.version && this.props.version.toJSON().endPage ? 
+        <Col md={12}>
+          <img src={this.props.version.toJSON().endPage.image} style={{margin:'auto'}} alt="" className="img-responsive"/>
+          <DisplayText isItalic={this.props.version.toJSON().endPage.isItalic} isBold={this.props.version.toJSON().endPage.isBold}>
+            {this.props.version.toJSON().endPage.text}
+          </DisplayText>
+          {
+            this.props.version.toJSON().endPage.buttonText !== '' &&
+            <DisplayButton>
+              <a className='btn btn-primary btn-lg' href='#'> {this.props.version.toJSON().endPage.buttonText} </a>
+            </DisplayButton>
+          }
+        </Col>
+        :
+        <div>
+          <h1>Thanks!</h1>
+          <p>Your response has been submitted.</p>
+        </div>
+      }
+      </Grid>
     );
   }
 }
 
-export default QuestionnaireFormSubmitted;
+function mapStateToProps(state, ownProps) {
+  const props = {
+    version: getCurrentVersion(state)
+  };
+  return props;
+}
+
+export default connect(mapStateToProps)(QuestionnaireFormSubmitted);
