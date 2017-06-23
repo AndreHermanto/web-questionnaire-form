d3d12d890a597ca3824b58cf97283865b0b8d5fb
andrehermanto <a.hermanto@garvan.org.au>
2017-06-23 13:26:02 +1000

    Adding proptypes I'm adding proptypes to Questionnaire admin just so that it is uniform with other UI component Closes: https://github.com/genome-one/sanford/issues/151

--- src/components/QuestionnaireAdmin.js
+++ src/components/QuestionnaireAdmin.js
@@ -1,37 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router';
+import PropTypes from 'prop-types';
 
-export default function QuestionnaireAdmin(props) {
+const propTypes = {
+  resume: PropTypes.bool.isRequired,
+  handleChangeResume: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  isError: PropTypes.bool.isRequired,
+  items: PropTypes.instanceOf(Immutable.List).isRequired
+};
+
+const defaultProps = {};
+
+function QuestionnaireAdmin(props) {
   return (
     <div className="container">
       <h4>Questionnaire Admin</h4>
-        <form>
-          <label>
-            Resume:
-            <input
-              name="resume"
-              type="checkbox"
-              checked={props.resume}
-              onChange={props.handleChangeResume}
+      <form>
+        <label>
+          Resume:
+          <input
+            name="resume"
+            type="checkbox"
+            checked={props.resume}
+            onChange={props.handleChangeResume}
           />
         </label>
       </form>
       <ul>
-        {props.isLoading &&
-          <p>Loading questionnaires...</p>
-        }
-        {props.isError &&
-          <p>Error loading quesitonnaires.</p>
-        }
-        {(props.items && props.items.length === 0) &&
-          <p className="text-muted">No questionnaires</p>
-        }
-        {props.items && props.items.map(questionnaire =>
-          (<li key={questionnaire.get('id')}>
-            <Link to={`/users/admin/questionnaires/${questionnaire.get('id')}?resume=${props.resume}&showlogic=true`}>{questionnaire.get('currentTitle')}</Link>
-          </li>)
-        )}
+        {props.isLoading && <p>Loading questionnaires...</p>}
+        {props.isError && <p>Error loading quesitonnaires.</p>}
+        {props.items &&
+          props.items.length === 0 &&
+          <p className="text-muted">No questionnaires</p>}
+        {props.items &&
+          props.items.map(questionnaire =>
+            <li key={questionnaire.get('id')}>
+              <Link
+                to={`/users/admin/questionnaires/${questionnaire.get(
+                  'id'
+                )}?resume=${props.resume}&showlogic=true`}
+              >
+                {questionnaire.get('currentTitle')}
+              </Link>
+            </li>
+          )}
       </ul>
     </div>
   );
 }
+
+QuestionnaireAdmin.propTypes = propTypes;
+QuestionnaireAdmin.defaultProps = defaultProps;
+
+export default QuestionnaireAdmin;
