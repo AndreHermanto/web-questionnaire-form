3ff92cfe4b2d40533ae8c19e7106b4fb7537a22b
andrehermanto <a.hermanto@garvan.org.au>
2017-06-27 14:08:42 +1000

    🆕Hide Prefer not to answer button if question is compulsory
    Basically if question is required it will check whether the question has been answered before it display buttons. I also removed unused import.
    
    https://github.com/genome-one/sanford/issues/157

--- src/components/Question.js
+++ src/components/Question.js
@@ -32,63 +32,87 @@ const Height = styled.input`
   float: left;
 `;
 
-function Question({
-  element,
-  number,
-  responseElement,
-  onAnswer,
-  showlogic
-}) {
-  if(!element) {
+const RequiredText = styled.div`
+  padding-left: 10px;
+  font-weight: 300;
+  color: #777;
+`;
+
+function Question({ element, number, responseElement, onAnswer, showlogic }) {
+  if (!element) {
     return <div>Loading Question...</div>;
   }
-  if(!responseElement) {
+  if (!responseElement) {
     return <div>Loading Response...</div>;
   }
   const handleAnswer = (e, answer) => {
     const target = e.target;
     if (element.get('type') === 'checkbox') {
       if (target.checked) {
-        return onAnswer(responseElement.update('answers', currentAnswers =>
-          currentAnswers.push(fromJS({ id: answer.get('id') })))
+        return onAnswer(
+          responseElement.update('answers', currentAnswers =>
+            currentAnswers.push(fromJS({ id: answer.get('id') }))
+          )
         );
       }
       const blah = responseElement.update('answers', currentAnswers =>
-        currentAnswers.filter(currentAnswer =>
-          currentAnswer.get('id') !== answer.get('id')
+        currentAnswers.filter(
+          currentAnswer => currentAnswer.get('id') !== answer.get('id')
         )
       );
       return onAnswer(blah);
     }
     if (element.get('type') === 'radio') {
-      return onAnswer(responseElement.set('answers', fromJS([{ id: answer.get('id') }])));
+      return onAnswer(
+        responseElement.set('answers', fromJS([{ id: answer.get('id') }]))
+      );
     }
     if (element.get('type') === 'text') {
-      const newResponseElement = responseElement.set('answers', fromJS([{
-        id: answer.get('id'),
-        text: target.value
-      }]));
+      const newResponseElement = responseElement.set(
+        'answers',
+        fromJS([
+          {
+            id: answer.get('id'),
+            text: target.value
+          }
+        ])
+      );
       return onAnswer(newResponseElement);
     }
     if (element.get('type') === 'weight') {
-      const newResponseElement = responseElement.set('answers', fromJS([{
-        id: answer.get('id'),
-        pounds: target.value !== '' ? parseInt(target.value, 10) : ''
-      }]));
+      const newResponseElement = responseElement.set(
+        'answers',
+        fromJS([
+          {
+            id: answer.get('id'),
+            pounds: target.value !== '' ? parseInt(target.value, 10) : ''
+          }
+        ])
+      );
       return onAnswer(newResponseElement);
     }
     if (element.get('type') === 'date') {
-      const newResponseElement = responseElement.set('answers', fromJS([{
-        id: answer.get('id'),
-        date: target.value
-      }]));
+      const newResponseElement = responseElement.set(
+        'answers',
+        fromJS([
+          {
+            id: answer.get('id'),
+            date: target.value
+          }
+        ])
+      );
       return onAnswer(newResponseElement);
     }
     if (element.get('type') === 'number') {
-      const newResponseElement = responseElement.set('answers', fromJS([{
-        id: answer.get('id'),
-        number: target.value !== '' ? parseInt(target.value, 10) : ''
-      }]));
+      const newResponseElement = responseElement.set(
+        'answers',
+        fromJS([
+          {
+            id: answer.get('id'),
+            number: target.value !== '' ? parseInt(target.value, 10) : ''
+          }
+        ])
+      );
       return onAnswer(newResponseElement);
     }
     return null;
@@ -97,19 +121,29 @@ function Question({
   const handleAnswerHeight = (e, answer, unit) => {
     const target = e.target;
     if (unit === 'feet') {
-      const newResponseElement = responseElement.set('answers', fromJS([{
-        id: answer.get('id'),
-        feet: target.value !== '' ? parseInt(target.value, 10) : '',
-        inches: responseElement.getIn(['answers', 0, 'inches'])
-      }]));
+      const newResponseElement = responseElement.set(
+        'answers',
+        fromJS([
+          {
+            id: answer.get('id'),
+            feet: target.value !== '' ? parseInt(target.value, 10) : '',
+            inches: responseElement.getIn(['answers', 0, 'inches'])
+          }
+        ])
+      );
       return onAnswer(newResponseElement);
     }
     if (unit === 'inches') {
-      const newResponseElement = responseElement.set('answers', fromJS([{
-        id: answer.get('id'),
-        inches: target.value !== '' ? parseInt(target.value, 10) : '',
-        feet: responseElement.getIn(['answers', 0, 'feet'])
-      }]));
+      const newResponseElement = responseElement.set(
+        'answers',
+        fromJS([
+          {
+            id: answer.get('id'),
+            inches: target.value !== '' ? parseInt(target.value, 10) : '',
+            feet: responseElement.getIn(['answers', 0, 'feet'])
+          }
+        ])
+      );
       return onAnswer(newResponseElement);
     }
     return null;
@@ -120,126 +154,212 @@ function Question({
   if (isQuestion(element)) {
     answers = element.get('answers').map((answer, answerIndex) => {
       if (element.get('type') === 'text') {
-        return (<div key={answer.get('id')}>
-          <textarea
-            key={answer.get('id')}
-            className="form-control"
-            rows="3"
-            value={coalesce(responseElement.getIn(['answers', answerIndex, 'text']), '')}
-            onChange={e => handleAnswer(e, answer)}
-          />
-        </div>);
+        return (
+          <div key={answer.get('id')}>
+            <textarea
+              key={answer.get('id')}
+              className="form-control"
+              rows="3"
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'text']),
+                ''
+              )}
+              onChange={e => handleAnswer(e, answer)}
+            />
+          </div>
+        );
       } else if (element.get('type') === 'weight') {
-        return (<div key={answer.get('id')}>
-          <Weight
-            type="number"
-            inputmode="numeric"
-            key={answer.get('id')}
-            className="form-control"
-            value={coalesce(responseElement.getIn(['answers', answerIndex, 'pounds']), 0)}
-            onChange={e => handleAnswer(e, answer)}
-          /><Description>Pounds</Description>
-        </div>);
+        return (
+          <div key={answer.get('id')}>
+            <Weight
+              type="number"
+              inputmode="numeric"
+              key={answer.get('id')}
+              className="form-control"
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'pounds']),
+                0
+              )}
+              onChange={e => handleAnswer(e, answer)}
+            />
+            <Description>Pounds</Description>
+          </div>
+        );
       } else if (element.get('type') === 'date') {
-        return (<div key={answer.get('id')}>
-          <input
-            type="date"
-            className="form-control"
-            value={coalesce(responseElement.getIn(['answers', answerIndex, 'date']), '')}
-            onChange={e => handleAnswer(e, answer)}
-          />
-        </div>);
+        return (
+          <div key={answer.get('id')}>
+            <input
+              type="date"
+              className="form-control"
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'date']),
+                ''
+              )}
+              onChange={e => handleAnswer(e, answer)}
+            />
+          </div>
+        );
       } else if (element.get('type') === 'number') {
-        return (<div key={answer.get('id')}>
-          <input
-            type="number"
-            className="form-control"
-            value={coalesce(responseElement.getIn(['answers', answerIndex, 'number']), '')}
-            onChange={e => handleAnswer(e, answer)}
-          />
-        </div>);
+        return (
+          <div key={answer.get('id')}>
+            <input
+              type="number"
+              className="form-control"
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'number']),
+                ''
+              )}
+              onChange={e => handleAnswer(e, answer)}
+            />
+          </div>
+        );
       } else if (element.get('type') === 'height') {
-        return (<div key={answer.get('id')}>
-          <Height
-            type="number"
-            className="form-control"
-            value={coalesce(responseElement.getIn(['answers', answerIndex, 'feet']), '')}
-            onChange={e => handleAnswerHeight(e, answer, 'feet')}
-          /><Description> Feet</Description>
-          <Height
-            type="number"
-            className="form-control"
-            value={coalesce(responseElement.getIn(['answers', answerIndex, 'inches']), '')}
-            onChange={e => handleAnswerHeight(e, answer, 'inches')}
-          /><Description> Inches</Description>
-        </div>);
+        return (
+          <div key={answer.get('id')}>
+            <Height
+              type="number"
+              className="form-control"
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'feet']),
+                ''
+              )}
+              onChange={e => handleAnswerHeight(e, answer, 'feet')}
+            />
+            <Description> Feet</Description>
+            <Height
+              type="number"
+              className="form-control"
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'inches']),
+                ''
+              )}
+              onChange={e => handleAnswerHeight(e, answer, 'inches')}
+            />
+            <Description> Inches</Description>
+          </div>
+        );
       }
 
-      const selected = responseElement.get('answers').filter(chosenAnswer => chosenAnswer.get('id') === answer.get('id')).size > 0;
+      const selected =
+        responseElement
+          .get('answers')
+          .filter(chosenAnswer => chosenAnswer.get('id') === answer.get('id'))
+          .size > 0;
 
-      return (<div className={element.get('type')} key={answer.get('id')}>
-        <AnswerOption active={selected}>
-          {element.get('type') !== 'text' &&
-            (<input
-              style={{ marginRight: 8 }}
-              name={`${responseElement.get('id')}-${answer.get('id')}`}
-              type={element.get('type')}
-              checked={selected}
-              onChange={e => handleAnswer(e, answer)}
-            />)
-          }
-          {' '}
-          {answer.get('text')} {(answer.get('goTo') && showlogic === true) && <small className="text-muted">Go to: {answer.getIn(['goTo', 'title'])}</small>}
-          {' '}
-          {answer.get('concepts') && !!answer.get('concepts').count() && showlogic &&
-          <span className="text-muted">
-          {answer.get('concepts') && !!answer.get('concepts').count() &&
-            <small className="text-muted text-success">
-              ({answer.get('concepts').map(concept => concept.get('label')).join(', ')})
-            </small>
-          }
-          </span>
-          }
-          <img src={answer.get('image')} alt="" className="img-responsive" />
+      return (
+        <div className={element.get('type')} key={answer.get('id')}>
+          <AnswerOption active={selected}>
+            {element.get('type') !== 'text' &&
+              <input
+                style={{ marginRight: 8 }}
+                name={`${responseElement.get('id')}-${answer.get('id')}`}
+                type={element.get('type')}
+                checked={selected}
+                onChange={e => handleAnswer(e, answer)}
+              />}
+            {' '}
+            {answer.get('text')}{' '}
+            {answer.get('goTo') &&
+              showlogic === true &&
+              <small className="text-muted">
+                Go to: {answer.getIn(['goTo', 'title'])}
+              </small>}
+            {' '}
+            {answer.get('concepts') &&
+              !!answer.get('concepts').count() &&
+              showlogic &&
+              <span className="text-muted">
+                {answer.get('concepts') &&
+                  !!answer.get('concepts').count() &&
+                  <small className="text-muted text-success">
+                    ({answer
+                      .get('concepts')
+                      .map(concept => concept.get('label'))
+                      .join(', ')})
+                  </small>}
+              </span>}
+            <img src={answer.get('image')} alt="" className="img-responsive" />
 
-          {selected && answer.get('followUp') &&
-            <div style={{ marginTop: 8 }}>
-              <strong>{answer.getIn(['followUp', 'question'])}</strong>
-              <textarea
-                className="form-control"
-                rows="3"
-                autoFocus
-                value={responseElement.getIn(['answers', answerIndex, 'followUp', 'text'])}
-                onChange={e => onAnswer(responseElement.setIn(['answers', answerIndex, 'followUp', 'text'], e.target.value).toJSON())}
-              />
-            </div>
-          }
-        </AnswerOption>
-      </div>);
+            {selected &&
+              answer.get('followUp') &&
+              <div style={{ marginTop: 8 }}>
+                <strong>{answer.getIn(['followUp', 'question'])}</strong>
+                <textarea
+                  className="form-control"
+                  rows="3"
+                  autoFocus
+                  value={responseElement.getIn([
+                    'answers',
+                    answerIndex,
+                    'followUp',
+                    'text'
+                  ])}
+                  onChange={e =>
+                    onAnswer(
+                      responseElement
+                        .setIn(
+                          ['answers', answerIndex, 'followUp', 'text'],
+                          e.target.value
+                        )
+                        .toJSON()
+                    )}
+                />
+              </div>}
+          </AnswerOption>
+        </div>
+      );
     });
   } else {
     answers = 'Unknown type';
   }
 
-  return (<div style={{ marginBottom: 24, backgroundColor: 'white', border: '1px solid #eee', padding: 32 }}>
-    <div className="media">
-      <div className="pull-left"><strong>{number}.</strong></div>
-      <div className="media-body" style={{ paddingLeft: 16 }}>
-        <p style={{ color: '#333', fontSize: 16, marginBottom: 16 }}>
-          <strong>{element.get('question').split('\n').map((item, key) => <span key={key}>{item}<br /></span>)}</strong>
-        </p>
-        {element.get('image') &&
-        <div style={{ border: '1px solid #eee', padding: '5px', marginBottom: '8px' }}>
-          <img src={element.get('image')} alt="" className="img-responsive" />
-        </div>}
-        <p className="text-muted">
-          {element.get('type') === 'radio' && <span>Select One</span>}
-          {element.get('type') === 'checkbox' && <span>Select Any</span>}
-        </p>
-        {answers}
+  return (
+    <div
+      style={{
+        marginBottom: 24,
+        backgroundColor: 'white',
+        border: '1px solid #eee',
+        padding: 32
+      }}
+    >
+      <div className="media">
+        <div className="pull-left"><strong>{number}.</strong></div>
+        <div className="media-body" style={{ paddingLeft: 16 }}>
+          <p style={{ color: '#333', fontSize: 16, marginBottom: 16 }}>
+            <strong>
+              {element
+                .get('question')
+                .split('\n')
+                .map((item, key) => <span key={key}>{item}<br /></span>)}
+            </strong>
+          </p>
+          {element.get('required') &&
+            <RequiredText>
+              <small>Required</small>
+            </RequiredText>}
+          {element.get('image') &&
+            <div
+              style={{
+                border: '1px solid #eee',
+                padding: '5px',
+                marginBottom: '8px'
+              }}
+            >
+              <img
+                src={element.get('image')}
+                alt=""
+                className="img-responsive"
+              />
+            </div>}
+          <p className="text-muted">
+            {element.get('type') === 'radio' && <span>Select One</span>}
+            {element.get('type') === 'checkbox' && <span>Select Any</span>}
+          </p>
+          {answers}
+        </div>
       </div>
     </div>
-  </div>);
+  );
 }
 
 export default Helpers.Element(Question);
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { fromJS } from 'immutable';
 import { coalesce } from '../helpers/coalesce';
 import { isQuestion } from '../helpers/questions';
 
@@ -31,16 +30,17 @@ const Height = styled.input`
   float: left;
 `;
 
-function Summary({
-  element,
-  number,
-  responseElement,
-  showlogic
-}) {
-  if(!element) {
+const RequiredText = styled.div`
+  padding-left: 10px;
+  font-weight: 300;
+  color: #777;
+`;
+
+function Summary({ element, number, responseElement, showlogic }) {
+  if (!element) {
     return <div>Loading Question...</div>;
   }
-  if(!responseElement) {
+  if (!responseElement) {
     return <div>Loading Response...</div>;
   }
 
@@ -49,126 +49,197 @@ function Summary({
   if (isQuestion(element)) {
     answers = element.get('answers').map((answer, answerIndex) => {
       if (element.get('type') === 'text') {
-        return (<div key={answer.get('id')}>
-          <textarea
-            key={answer.get('id')}
-            className="form-control"
-            rows="3"
-            value={coalesce(responseElement.getIn(['answers', answerIndex, 'text']), '')}
-            readOnly
-          />
-        </div>);
+        return (
+          <div key={answer.get('id')}>
+            <textarea
+              key={answer.get('id')}
+              className="form-control"
+              rows="3"
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'text']),
+                ''
+              )}
+              readOnly
+            />
+          </div>
+        );
       } else if (element.get('type') === 'weight') {
-          return (<div key={answer.get('id')}>
+        return (
+          <div key={answer.get('id')}>
             <Weight
               type="number"
               inputmode="numeric"
               key={answer.get('id')}
               className="form-control"
-              value={coalesce(responseElement.getIn(['answers', answerIndex, 'pounds']), 0)}
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'pounds']),
+                0
+              )}
               readOnly
-            /><Description>Pounds</Description>
-          </div>);
+            />
+            <Description>Pounds</Description>
+          </div>
+        );
       } else if (element.get('type') === 'date') {
-          return (<div key={answer.get('id')}>
+        return (
+          <div key={answer.get('id')}>
             <input
               type="date"
               className="form-control"
-              value={coalesce(responseElement.getIn(['answers', answerIndex, 'date']), '')}
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'date']),
+                ''
+              )}
               readOnly
             />
-          </div>);
+          </div>
+        );
       } else if (element.get('type') === 'number') {
-          return (<div key={answer.get('id')}>
+        return (
+          <div key={answer.get('id')}>
             <input
               type="number"
               className="form-control"
-              value={coalesce(responseElement.getIn(['answers', answerIndex, 'number']), '')}
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'number']),
+                ''
+              )}
               readOnly
             />
-            </div>);      
+          </div>
+        );
       } else if (element.get('type') === 'height') {
-          return (<div key={answer.get('id')}>
+        return (
+          <div key={answer.get('id')}>
             <Height
               type="number"
               className="form-control"
-              value={coalesce(responseElement.getIn(['answers', answerIndex, 'feet']), '')}
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'feet']),
+                ''
+              )}
               readOnly
-            /><Description> Feet</Description>
+            />
+            <Description> Feet</Description>
             <Height
               type="number"
               className="form-control"
-              value={coalesce(responseElement.getIn(['answers', answerIndex, 'inches']), '')}
+              value={coalesce(
+                responseElement.getIn(['answers', answerIndex, 'inches']),
+                ''
+              )}
               readOnly
-            /><Description> Inches</Description>
-          </div>);
+            />
+            <Description> Inches</Description>
+          </div>
+        );
       }
 
-      const selected = responseElement.get('answers').filter(chosenAnswer => chosenAnswer.get('id') === answer.get('id')).size > 0;
+      const selected =
+        responseElement
+          .get('answers')
+          .filter(chosenAnswer => chosenAnswer.get('id') === answer.get('id'))
+          .size > 0;
 
-      return (<div className={element.get('type')} key={answer.get('id')}>
-        <AnswerOption active={selected}>
-          {element.get('type') !== 'text' &&
-            (<input
-              style={{ marginRight: 8 }}
-              name={`${responseElement.get('id')}-${answer.get('id')}`}
-              type={element.get('type')}
-              checked={selected}
-              readOnly
-            />)
-          }
-          {' '}
-          {answer.get('text')} {(answer.get('goTo') && showlogic === true) && <small className="text-muted">Go to: {answer.getIn(['goTo', 'title'])}</small>}
-          {' '}
-          {answer.get('concepts') && !!answer.get('concepts').count() && showlogic &&
-          <span className="text-muted">
-          {answer.get('concepts') && !!answer.get('concepts').count() &&
-            <small className="text-muted text-success">
-              ({answer.get('concepts').map(concept => concept.get('label')).join(', ')})
-            </small>
-          }
-          </span>
-          }
-          <img src={answer.get('image')} alt="" className="img-responsive" />
-
-          {selected && answer.get('followUp') &&
-            <div style={{ marginTop: 8 }}>
-              <strong>{answer.getIn(['followUp', 'question'])}</strong>
-              <textarea
-                className="form-control"
-                rows="3"
-                autoFocus
-                value={responseElement.getIn(['answers', answerIndex, 'followUp', 'text'])}
+      return (
+        <div className={element.get('type')} key={answer.get('id')}>
+          <AnswerOption active={selected}>
+            {element.get('type') !== 'text' &&
+              <input
+                style={{ marginRight: 8 }}
+                name={`${responseElement.get('id')}-${answer.get('id')}`}
+                type={element.get('type')}
+                checked={selected}
                 readOnly
-              />
-            </div>
-          }
-        </AnswerOption>
-      </div>);
+              />}
+            {' '}
+            {answer.get('text')}{' '}
+            {answer.get('goTo') &&
+              showlogic === true &&
+              <small className="text-muted">
+                Go to: {answer.getIn(['goTo', 'title'])}
+              </small>}
+            {' '}
+            {answer.get('concepts') &&
+              !!answer.get('concepts').count() &&
+              showlogic &&
+              <span className="text-muted">
+                {answer.get('concepts') &&
+                  !!answer.get('concepts').count() &&
+                  <small className="text-muted text-success">
+                    ({answer
+                      .get('concepts')
+                      .map(concept => concept.get('label'))
+                      .join(', ')})
+                  </small>}
+              </span>}
+            <img src={answer.get('image')} alt="" className="img-responsive" />
+
+            {selected &&
+              answer.get('followUp') &&
+              <div style={{ marginTop: 8 }}>
+                <strong>{answer.getIn(['followUp', 'question'])}</strong>
+                <textarea
+                  className="form-control"
+                  rows="3"
+                  autoFocus
+                  value={responseElement.getIn([
+                    'answers',
+                    answerIndex,
+                    'followUp',
+                    'text'
+                  ])}
+                  readOnly
+                />
+              </div>}
+          </AnswerOption>
+        </div>
+      );
     });
   } else {
     answers = 'Unknown type';
   }
 
-  return (<div style={{ marginBottom: 15 }}>
-    <div className="media">
-      <div className="pull-left"><strong>{number}.</strong></div>
-      <div className="media-body" style={{ paddingLeft: 10 }}>
-        <p style={{ color: '#333', fontSize: 16, marginBottom: 6 }}>
-          <strong>{element.get('question').split('\n').map((item, key) => <span key={key}>{item}<br /></span>)}</strong>
-        </p>
-        {element.get('image') &&
-        <div style={{ border: '1px solid #eee', padding: '5px', marginBottom: '8px' }}>
-          <img src={element.get('image')} alt="" className="img-responsive" />
-        </div>}
-        <p className="text-muted">
-          {element.get('type') === 'radio' && <span>Select One</span>}
-          {element.get('type') === 'checkbox' && <span>Select Any</span>}
-        </p>
-        {answers}
+  return (
+    <div style={{ marginBottom: 15 }}>
+      <div className="media">
+        <div className="pull-left"><strong>{number}.</strong></div>
+        <div className="media-body" style={{ paddingLeft: 10 }}>
+          <p style={{ color: '#333', fontSize: 16, marginBottom: 6 }}>
+            <strong>
+              {element
+                .get('question')
+                .split('\n')
+                .map((item, key) => <span key={key}>{item}<br /></span>)}
+            </strong>
+          </p>
+          {element.get('required') &&
+            <RequiredText>
+              <small>Required</small>
+            </RequiredText>}
+          {element.get('image') &&
+            <div
+              style={{
+                border: '1px solid #eee',
+                padding: '5px',
+                marginBottom: '8px'
+              }}
+            >
+              <img
+                src={element.get('image')}
+                alt=""
+                className="img-responsive"
+              />
+            </div>}
+          <p className="text-muted">
+            {element.get('type') === 'radio' && <span>Select One</span>}
+            {element.get('type') === 'checkbox' && <span>Select Any</span>}
+          </p>
+          {answers}
+        </div>
       </div>
     </div>
-  </div>);
+  );
 }
 
 export default Summary;
