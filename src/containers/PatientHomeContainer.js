c7b28f3b6b1678d7aaec7d205ba3101ecd3315ce
Craig McNamara <craig.mcnamara@genome.one>
2017-07-26 20:57:57 +1000

    remove unused code

--- src/containers/PatientHomeContainer.js
+++ src/containers/PatientHomeContainer.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  fetchQuestionnaires,
-  setResume,
-  fetchConsentTypeMappings,
-  fetchDataForHomepage
-} from '../actions';
+import { fetchDataForHomepage } from '../actions';
 
 import QuestionnaireDashboard from '../components/QuestionnaireDashboard';
 import Footer from '../components/Footer';
-import { getQuestionnaires, getHomepageQuestionnaires } from '../reducers';
+import { getHomepageQuestionnaires } from '../reducers';
 
 class PatientHomeContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChangeResume = this.handleChangeResume.bind(this);
-  }
   componentDidMount() {
-    // get out consent type
-    // load consent type mapping
-    // const consentTypeId = '83ffdd60-5098-49f6-8f77-635293df18e4';
-    // const userId = 'admin';
     const { userId, consentTypeId } = this.props.params;
     this.props.dispatch(fetchDataForHomepage(consentTypeId, userId));
-    // get the questionnaire
-    // load that
-    // fetch responses for consent type id
-    // fetch versions for those responses
-    // this.props.dispatch(fetchQuestionnaires());
-  }
-  handleChangeResume() {
-    this.props.dispatch(setResume(!this.props.resume));
   }
   render() {
     const { questionnaires } = this.props;
