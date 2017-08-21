33cdbc6b0176aa43acd4018d0a3118abfe0572bd
Luong Khoi Nguyen Nguyen <khoi.nguyen@genome.one>
2017-08-21 13:19:53 +1000

    minor change for Date.js!

--- src/components/elements/Date.js
+++ src/components/elements/Date.js
@@ -3,6 +3,11 @@ import get from 'lodash.get';
 class Date extends Component {
   render() {
     const { answers, setAnswerValue, responseElementAnswers } = this.props;
+    const preventTypingMinus = e => {
+      if (e.keyCode === 189) {
+        e.preventDefault();
+      }
+    };
     return (
       <div>
         <div className="row">
@@ -51,11 +56,7 @@ class Date extends Component {
                     'day',
                     e.target.value.slice(0, 2)
                   )}
-                onKeyDown={e => {
-                  if (e.keyCode === 189) {
-                    e.preventDefault();
-                  }
-                }}
+                onKeyDown={preventTypingMinus}
               />
             </div>
           </div>
@@ -74,11 +75,7 @@ class Date extends Component {
                     'year',
                     e.target.value.slice(0, 4)
                   )}
-                onKeyDown={e => {
-                  if (e.keyCode === 189) {
-                    e.preventDefault();
-                  }
-                }}
+                onKeyDown={preventTypingMinus}
               />
             </div>
           </div>
