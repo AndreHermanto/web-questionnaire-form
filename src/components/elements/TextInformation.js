1bcafb93e00847d7facf2da3074eea3bb113bd2b
Luong Khoi Nguyen Nguyen <khoi.nguyen@genome.one>
2017-08-23 12:17:15 +1000

    added markdown for displaying bold text!

--- src/components/elements/TextInformation.js
+++ src/components/elements/TextInformation.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
+import Markdown from 'react-markdown';
 class TextInformation extends Component {
   render() {
     const { element } = this.props;
-    return (
-      <div
-        style={{
-          fontWeight: `${element.textStyle.isBold ? 'bold' : 'normal'}`
-        }}
-      >
-        {element.text}
-      </div>
-    );
+    return <Markdown source={element.text} escapeHtml={true} skipHtml={true} />;
   }
 }
 
