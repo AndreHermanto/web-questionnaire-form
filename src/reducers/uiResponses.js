3672723bdf6512e540b5a5fb64c1daf5da688a84
Ali Azarkhish <ali.azarkhish@genome.one>
2017-09-08 09:40:15 +1000

    Issue #131 Add toast when PUT response is successful or fails

--- src/reducers/uiResponses.js
+++ src/reducers/uiResponses.js
@@ -3,6 +3,7 @@ import * as types from '../constants/ActionTypes';
 
 const initialState = fromJS({
   isLoading: false,
+  isResponseUpdating: false,
   isError: false,
   showPreferNotToAnswerModal: null
 });
@@ -15,13 +16,19 @@ const uiResponses = (state = initialState, action) => {
       return state.set('isLoading', false).set('isError', false);
     case types.FETCH_RESPONSES_FAILURE:
       return state.set('isLoading', false).set('isError', true);
+    case types.UPDATE_RESPONSE_REQUEST:
+      return state.set('isResponseUpdating', true);
     case types.SUBMIT_RESPONSE:
       return state.set('isLoading', true).set('isError', false);
     case types.UPDATE_RESPONSE_SUCCESS:
-      return state.set('isLoading', false).set('isError', false);
+      return state
+        .set('isLoading', false)
+        .set('isError', false)
+        .set('isResponseUpdating', false);
     case types.UPDATE_RESPONSE_FAILURE:
       return state
         .set('isLoading', false)
+        .set('isResponseUpdating', false)
         .set('isError', true)
         .set('submitResponseFailure', action.payload);
     case types.OPEN_PREFER_NOT_TO_ANSWER_MODAL:
