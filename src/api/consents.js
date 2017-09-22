4af77bbd9dbcafb6f3a6c01f6965c13a0075635d
Craig McNamara <craig.mcnamara@genome.one>
2017-09-22 20:07:32 +1000

    change get consent tpye mapping url
    Problem: argument not url encoded
    
    Solution: add encoding

--- src/api/consents.js
+++ src/api/consents.js
@@ -2,5 +2,5 @@ import * as rest from './rest';
 
 export const getConsentTypeMappings = consentTypeId =>
   rest.get(
-    `${process.env.REACT_APP_BASE_URL}/consent-type-mappings?consentTypeId=${consentTypeId}`
+    `${process.env.REACT_APP_BASE_URL}/consent-type-mappings?consentTypeId=${encodeURIComponent(consentTypeId)}`
   );
