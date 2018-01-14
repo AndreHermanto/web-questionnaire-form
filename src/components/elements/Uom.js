c37fb131131e206b8a7c39e5f5d85676f2a87fba
Craig McNamara <craig.mcnamara@genome.one>
2018-01-15 09:10:02 +1000

    Revert "Merge branch 'feature/add-units' into develop"
    Temporarily remove UOMs.
    
    2134d60c201058fddefa3fb591d46861890c2b6d

--- src/components/elements/Uom.js
+++ src/components/elements/Uom.js
@@ -5,19 +5,14 @@ class Uom extends Component {
     const { answers, setAnswerValue, responseElementAnswers } = this.props;
     return (
       <div>
-        <div className="input-group" style={{ marginBottom: 10 }}>
-          <input
-            type="number"
-            className="form-control"
-            value={get(responseElementAnswers, `${answers[0].id}.value1`, '')}
-            onChange={e =>
-              setAnswerValue(answers[0].id, 'value1', e.target.value)
-            }
-          />
-          <span className="input-group-addon" id="basic-addon2">
-            {answers[0].value1.uom.label}
-          </span>
-        </div>
+        <input
+          type="number"
+          placeholder={answers[0].value1.uom.label}
+          className="form-control"
+          value={get(responseElementAnswers, `${answers[0].id}.value1`, '')}
+          onChange={e =>
+            setAnswerValue(answers[0].id, 'value1', e.target.value)}
+        />
       </div>
     );
   }
